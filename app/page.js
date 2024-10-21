18bde60d243185312f81b52b561bcc56837e2d81
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-10-21 22:59:07 +0530

    added user

--- app/page.js
+++ app/page.js
@@ -312,6 +312,8 @@ const Page = () => {
                   { name: "Vishwas Mishra", image: "/vis.jpeg" },
                   { name: "Somdatta Pradhan", image: "/som.jpeg" },
                   { name: "Ankur Suman", image: "/ank.jpeg" },
+                  { name: "Arvasu Gupta", image: "/ars.jpeg" },
+
                 ].map((member, index) => (
                   <motion.div
                     key={index}
